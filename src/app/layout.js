d792174ebc0d12df5693557ef32eea6cddaf2344
Matheus Alves <matheus.alves.stela@gmail.com>
2025-01-01 19:31:27 -0300

    ajuste de padding no eixo x do layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import '../app/globals.css';
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center py-20">
+    <div className="min-h-screen flex flex-col justify-center items-center px-5 py-20">
       {children}
     </div>
   );
