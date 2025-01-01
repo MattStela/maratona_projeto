ed5c0ad8ab08b7011f887213a43d8f2de197c8e8
Matheus Alves <matheus.alves.stela@gmail.com>
2025-01-01 20:35:29 -0300

    atualizei e organizei algumas páginas

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,57 @@
-import Link from "next/link";
-import React from "react";
-import Layout from "@/app/layout"; // Importando o Layout da pasta correta
+// src/pages/plano.js
+import React from 'react';
+import Card from '@/components/card';
+import Layout from "@/app/layout";
 
-const Home = () => {
+const Plano = () => {
+  const cardData = [
+    {
+      title: "Janeiro a Março de 2025",
+      description: "Objetivo: Desenvolver a base aeróbica e melhorar a resistência.",
+      imageUrl: "/images/correndo1.png", // URL da imagem local
+      tags: ["Janeiro", "Fevereiro", "Março"],
+      link: "/jan_mar" // Corrigido para garantir que o link aponte para a página correta
+    },
+    {
+      title: "Abril a Junho de 2025: Construção de Resistência",
+      description: "Objetivo: Aumentar a distância de corrida e melhorar a capacidade aeróbica.",
+      imageUrl: "/images/correndo2.png", // URL da imagem local
+      tags: ["Abril", "Maio"],
+      link: "/abr_mai"
+    },
+    {
+      title: "Julho de 2025: Preparação Final",
+      description: "Objetivo: Manter a forma e evitar lesões.",
+      imageUrl: "/images/correndo3.png", // URL da imagem local
+      tags: ["Junho"],
+      link: "/julho"
+    },
+    {
+        title: "Rota atual da Maratona (sujeito a mudança)",
+        description: "A rota de 10km com os pontos de parada a cada 1km",
+        imageUrl: "/images/map.png", // URL da imagem local
+        tags: ["Junho"],
+        link: "https://www.google.com.br/maps/dir/R.+Noroguages,+211+-+Itaquera,+S%C3%A3o+Paulo+-+SP,+08215-670,+Brasil/-23.5565399,-46.4462129/-23.5620404,-46.450605/-23.5632773,-46.460745/-23.5657733,-46.4678866/-23.5714655,-46.4662935/-23.572307,-46.4647265/-23.5782374,-46.4588428/-23.5700831,-46.4593192/-23.561991,-46.4508039/@-23.5645671,-46.4589294,14.75z/data=!4m17!4m16!1m5!1m1!1s0x94ce67d77cbe9339:0x418829ed82fc01c6!2m2!1d-46.4431479!2d-23.5499751!1m0!1m0!1m0!1m0!1m0!1m0!1m0!1m0!1m0!3e2?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
+      }
+    
+  ];
+
   return (
     <Layout>
-      <div className="text-center w-3/4">
-        <h1>
-          A idéia inicial é planejar e fazer os treinos até concluir o objetivo
-          de correr 10km na maratona que ocorrerá entre dia 29/06 e dia 05/06
-        </h1>
-        <br></br>
-        <h1>Para saber mais sobre o plano de treino para a maratona,</h1>
-        <Link href="/plano" className="hover:text-blue-500 hover:font-bold hover:cursor-pointer">
-            clique aqui
-        </Link>
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            description={card.description}
+            imageUrl={card.imageUrl}
+            tags={card.tags}
+            link={card.link}
+          />
+        ))}
       </div>
     </Layout>
   );
 };
 
-export default Home;
+export default Plano;
