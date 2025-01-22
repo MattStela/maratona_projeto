1ee56e8a7b41d8717f4c5853c2da64b1174124f3
Matheus Alves <matheus.alves.stela@gmail.com>
2025-01-22 10:45:03 -0300

    adicionado gráfico de clima

---
+++ src/components/graficos.js
@@ -0,0 +1,147 @@
+// grafico.js
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend
+);
+
+export default function Graficos() {
+  const [forecastDataTemp, setForecastDataTemp] = useState(null);
+  const [forecastDataHumidity, setForecastDataHumidity] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const urlSP = `https://api.open-meteo.com/v1/forecast?latitude=-23.56&longitude=-46.63&hourly=temperature_2m,relative_humidity_2m`;
+      const urlItaquera = `https://api.open-meteo.com/v1/forecast?latitude=-23.60&longitude=-46.58&hourly=temperature_2m,relative_humidity_2m`;
+
+      try {
+        const [resSP, resItaquera] = await Promise.all([
+          fetch(urlSP),
+          fetch(urlItaquera),
+        ]);
+
+        if (!resSP.ok || !resItaquera.ok) {
+          throw new Error("Falha ao buscar dados da API");
+        }
+
+        const dataSP = await resSP.json();
+        const dataItaquera = await resItaquera.json();
+
+        const temperaturesSP = dataSP.hourly.temperature_2m.slice(0, 24);
+        const humiditySP = dataSP.hourly.relative_humidity_2m.slice(0, 24);
+        const temperaturesItaquera = dataItaquera.hourly.temperature_2m.slice(0, 24);
+        const humidityItaquera = dataItaquera.hourly.relative_humidity_2m.slice(0, 24);
+
+        setForecastDataTemp({
+          labels: Array.from({ length: 24 }, (_, i) => `${i}h`),
+          datasets: [
+            {
+              label: "Temperatura SP",
+              data: temperaturesSP,
+              borderColor: "rgb(240, 69, 43)",
+              backgroundColor: "rgba(77, 0, 0, 0.2)",
+              fill: true,
+              yAxisID: 'y-temp',
+            },
+            {
+              label: "Temperatura Itaquera",
+              data: temperaturesItaquera,
+              borderColor: "rgb(255, 161, 10)",
+              backgroundColor: "rgba(153, 102, 255, 0.2)",
+              fill: true,
+              yAxisID: 'y-temp',
+            },
+          ],
+        });
+
+        setForecastDataHumidity({
+          labels: Array.from({ length: 24 }, (_, i) => `${i}h`),
+          datasets: [
+            {
+              label: "Umidade SP",
+              data: humiditySP,
+              borderColor: "rgb(4, 169, 251)",
+              backgroundColor: "rgba(75, 75, 192, 0.2)",
+              fill: true,
+              yAxisID: 'y-humidity',
+            },
+            {
+              label: "Umidade Itaquera",
+              data: humidityItaquera,
+              borderColor: "rgb(2, 94, 255)",
+              backgroundColor: "rgba(153, 153, 255, 0.2)",
+              fill: true,
+              yAxisID: 'y-humidity',
+            },
+          ],
+        });
+      } catch (error) {
+        console.error("Erro ao buscar dados da API:", error);
+      }
+    };
+
+    fetchWeather(); // Chama a função na montagem do componente
+  }, []);
+
+  const options = {
+    plugins: {
+      legend: {
+        display: true,
+      },
+      tooltip: {
+        mode: 'index',
+        intersect: false,
+      },
+    },
+    scales: {
+      x: {
+        display: false,
+      },
+      'y-temp': {
+        type: 'linear',
+        display: false,
+        position: 'left',
+      },
+      'y-humidity': {
+        type: 'linear',
+        display: false,
+        position: 'right',
+        grid: {
+          drawOnChartArea: false, // only want the grid lines for one axis
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="flex justify-center items-center relative w-full">
+      {forecastDataHumidity && forecastDataTemp && (
+        <div className="w-[90%]">
+          <Line className="w-full" data={{
+            labels: forecastDataTemp.labels,
+            datasets: [
+              ...forecastDataTemp.datasets,
+              ...forecastDataHumidity.datasets,
+            ],
+          }} options={options} />
+        </div>
+      )}
+      {!forecastDataTemp && !forecastDataHumidity && <p>Carregando dados de previsão...</p>}
+    </div>
+  );
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,98 +1,38 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import Graficos from "./graficos";
 
 export default function Header() {
   const [temperaturaSP, setTemperaturaSP] = useState(null);
   const [temperaturaItaquera, setTemperaturaItaquera] = useState(null);
 
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const lastFetchTime = localStorage.getItem('lastFetchTime');
-      const currentTime = new Date().getTime();
-      const oneHour = 60 * 60 * 1000;
-
-      const fetchNewData = async () => {
-        const urlSP = `https://api.open-meteo.com/v1/forecast?latitude=-23.56&longitude=-46.63&current_weather=true`;
-        const urlItaquera = `https://api.open-meteo.com/v1/forecast?latitude=-23.60&longitude=-46.58&current_weather=true`;
-
-        try {
-          const [resSP, resItaquera] = await Promise.all([
-            fetch(urlSP),
-            fetch(urlItaquera),
-          ]);
-
-          if (!resSP.ok || !resItaquera.ok) {
-            throw new Error("Falha ao buscar dados da API");
-          }
-
-          const dataSP = await resSP.json();
-          const dataItaquera = await resItaquera.json();
-
-          if (dataSP.current_weather && dataItaquera.current_weather) {
-            const tempSP = dataSP.current_weather.temperature;
-            const tempItaquera = dataItaquera.current_weather.temperature;
-
-            setTemperaturaSP(tempSP);
-            setTemperaturaItaquera(tempItaquera);
-
-            const weatherData = {
-              temperaturaSP: tempSP,
-              temperaturaItaquera: tempItaquera,
-              fetchTime: currentTime,
-            };
-            localStorage.setItem('weatherData', JSON.stringify(weatherData));
-            localStorage.setItem('lastFetchTime', currentTime.toString());
-          } else {
-            console.error("Dados da API não contêm 'current_weather'");
-          }
-        } catch (error) {
-          console.error("Erro ao buscar dados da API:", error);
-        }
-      };
-
-      if (!lastFetchTime) {
-        fetchNewData(); // Primeira chamada na montagem do componente
-      } else {
-        const weatherDataString = localStorage.getItem('weatherData');
-        if (!weatherDataString || currentTime - lastFetchTime > oneHour) {
-          fetchNewData(); // Chama a função se os dados do clima não estiverem no localStorage ou já passaram uma hora
-        } else {
-          const weatherData = JSON.parse(weatherDataString);
-          setTemperaturaSP(weatherData.temperaturaSP);
-          setTemperaturaItaquera(weatherData.temperaturaItaquera);
-        }
-      }
-    };
-
-    fetchWeather(); // Chama a função na montagem do componente
-
-  }, []);
-
   return (
-    <div className="mb-10 w-full h-32 flex flex-col items-center justify-center text-gray-400">
-      <div className="menu flex flex-row h-[70%] items-center justify-center w-full">
+    <div className="w-full flex flex-col items-center justify-center text-gray-400">
+      <div className="menu flex flex-row items-center justify-center w-full">
         <div className="h-full w-[20%] flex-grow flex flex-col justify-center items-center">
           <Image
             src="/images/logo-no-bg.png"
             alt="Logo"
             width={100}
             height={100}
-            className="w-full h-full object-contain"
+            className="object-contain"
           />
         </div>
         <div className="h-full w-[60%] flex-grow flex flex-col justify-center items-center">
-          <Link href="/#" className="hover:text-white text-gray-500 font-semibold py-2 px-4 rounded transition duration-300">
+          <Link
+            href="/#"
+            className="hover:text-white text-gray-500 font-semibold py-2 px-4 rounded transition duration-300"
+          >
             Início
           </Link>
         </div>
         <div className="h-full w-[20%] flex-grow flex flex-col justify-center items-center"></div>
       </div>
       <br />
-      <div className="informativo h-[30%] flex flex-col items-center justify-center w-[80%] text-center">
-        <p>Clima hoje em SP: {temperaturaSP !== null ? `${temperaturaSP}` : "Carregando..."}ºC</p>
-        <p>Itaquera: {temperaturaItaquera !== null ? `${temperaturaItaquera}` : "Carregando..."}ºC</p>
+      <div className="m-10 informativo flex flex-col items-center justify-center w-full text-center">
+        <Graficos />
       </div>
     </div>
   );
